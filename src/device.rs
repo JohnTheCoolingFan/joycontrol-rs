93a2a6441f190c733a2382e15e89b5fa103428f3
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-17 23:15:17 +0300

    Fix errors and make it sound

--- src/device.rs
+++ src/device.rs
@@ -1,23 +1,22 @@
-use btleplug::api::{Central, BDAddr, Manager as _, Peripheral as _};
+use btleplug::api::{BDAddr, Central, Manager as _, Peripheral as _};
 use btleplug::platform::{Adapter, Manager, Peripheral};
-use uuid::{uuid, Uuid};
 use std::thread;
 use tokio;
+use uuid::{uuid, Uuid};
 
 const HID_UUID: Uuid = uuid!("00001124-0000-1000-8000-00805f9b34fb");
 
 struct HidDevice {
-    device_id: BDAddr
-    //TODO In original code its use Bluez DeviceID https://docs.rs/bluez-async/latest/bluez_async/struct.DeviceId.html i don't know how clone it
+    device_id: BDAddr, //TODO In original code its use Bluez DeviceID https://docs.rs/bluez-async/latest/bluez_async/struct.DeviceId.html i don't know how clone it
 }
-#[tokio::main]
+
 impl HidDevice {
-    async fn new(device_id: Option<&str>) -> HidDevice {
-        let manager = Manager::new().await.unwrap();
+    async fn new(device_id: Option<&str>) -> Result<HidDevice, btleplug::Error> {
+        let manager = Manager::new().await?;
 
         // get the first bluetooth adapter
         let adapters = manager.adapters().await?;
         let central = adapters.into_iter().nth(0).unwrap();
+        todo!()
     }
 }
-
