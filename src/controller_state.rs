7aa07cd1f5753c618e31ecb7794611e3ebb2c1de
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-25 10:09:59 +0300

    Constructor for ControllerState

--- src/controller_state.rs
+++ src/controller_state.rs
@@ -1,10 +1,12 @@
 use crate::{
     button_state::ButtonState, controller::Controller, memory::FlashMemory, nfc_tag::NFCTag,
-    stick_state::StickState,
+    stick_calibration::StickCalibration, stick_state::StickState,
 };
 
 // Protocol is ignored for now because that causes cyclic referencing or self-referencing, which is
 // hard  and there is probably a better way to do what the original code does
+//
+// sig_is_send omitted for now as it's unclear how it is used, alternative will be found later
 #[derive(Debug)]
 pub struct ControllerState {
     controller: Controller,
@@ -17,6 +19,51 @@ pub struct ControllerState {
 
 impl ControllerState {
     pub fn new(controller: Controller, spi_flash: Option<FlashMemory>) -> Self {
-        todo!()
+        let button_state = ButtonState::new(controller);
+
+        let l_stick_state: Option<StickState> =
+            (matches!(controller, Controller::ProController | Controller::JoyconL)).then(|| {
+                let calibration = if let Some(flash) = &spi_flash {
+                    let calibration_data = flash
+                        .get_user_l_stick_calibration()
+                        .unwrap_or_else(|| flash.get_factory_l_stick_calibration());
+                    Some(StickCalibration::l_from_bytes(
+                        &calibration_data.try_into().unwrap(),
+                    ))
+                } else {
+                    None
+                };
+                let mut l_stick_state = StickState::new(None, None, calibration).unwrap();
+                let _ = l_stick_state.set_center(); // Ignoring the error that would happen if
+                                                    // calibration is None
+                l_stick_state
+            });
+
+        let r_stick_state: Option<StickState> =
+            (matches!(controller, Controller::ProController | Controller::JoyconR)).then(|| {
+                let calibration = if let Some(flash) = &spi_flash {
+                    let calibration_data = flash
+                        .get_user_r_stick_calibration()
+                        .unwrap_or_else(|| flash.get_factory_r_stick_calibration());
+                    Some(StickCalibration::r_from_bytes(
+                        &calibration_data.try_into().unwrap(),
+                    ))
+                } else {
+                    None
+                };
+                let mut r_stick_state = StickState::new(None, None, calibration).unwrap();
+                let _ = r_stick_state.set_center(); // Ignoring the error that would happen if
+                                                    // calibration is None
+                r_stick_state
+            });
+
+        Self {
+            controller,
+            nfc_content: None,
+            spi_flash,
+            button_state,
+            l_stick_state,
+            r_stick_state,
+        }
     }
 }
