4fa856dea3cec64146edfe94c50d3698ac05dcfa
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-25 11:42:34 +0300

    Added MCU power state and crc calc

--- src/mcu.rs
+++ src/mcu.rs
@@ -1,3 +1,21 @@
+use crc::{Crc, CRC_8_SMBUS};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum MCUPowerState {
+    Suspended = 0x00,
+    Ready = 0x01,
+    ReadyUpdate = 0x02,
+    ConfiguredNFC = 0x04,
+    // ConfiguredIR = 0x05,
+    // COnfiguredUpdate = 0x06
+}
+
+pub fn mcu_crc(data: &[u8]) -> u8 {
+    let crc_calculator = Crc::<u8>::new(&CRC_8_SMBUS);
+    crc_calculator.checksum(data)
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(u8)]
 pub enum NFCState {
