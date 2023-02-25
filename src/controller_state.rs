a82ed86e8eacca9c4f179ef0a88125eb70296f03
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-25 08:55:59 +0300

    Some edits continuing  the rewrite

--- src/controller_state.rs
+++ src/controller_state.rs
@@ -1,10 +1,19 @@
-use crate::{button_state::ButtonState, controller::Controller, mcu::NFCTag, memory::FlashMemory};
+use crate::{
+    button_state::ButtonState, controller::Controller, memory::FlashMemory, nfc_tag::NFCTag,
+};
 
 #[derive(Debug)]
 pub struct ControllerState {
-    protocol: ControllerProtocol,
     controller: Controller,
     nfc_content: Option<NFCTag>,
     spi_flash: Option<FlashMemory>,
     button_state: ButtonState,
+    l_stick_state: Option<StickState>,
+    r_stick_state: Option<StickState>,
+}
+
+impl ControllerState {
+    pub fn new(controller: Controller, spi_flash: Option<FlashMemory>) -> Self {
+        todo!()
+    }
 }
