2e3b76f17f033a0243ec2a61b58986222d071f3e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-25 12:08:28 +0300

    MCU struct

--- src/mcu.rs
+++ src/mcu.rs
@@ -1,4 +1,16 @@
 use crc::{Crc, CRC_8_SMBUS};
+use lazy_static::lazy_static;
+use log::warn;
+use uuid::Uuid;
+
+use crate::{controller::Controller, nfc_tag::NFCTag};
+
+lazy_static! {
+    pub static ref REMOVE_AMIIBO: NFCTag = NFCTag::new(&[0; 540], None, None);
+    pub static ref NO_RESPONSE_MESSAGE: Vec<u8> = pack_message(&[0xFF], None, None, None);
+}
+
+const MAX_RESPONSE_QUEUE_LEN: usize = 4;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(u8)]
@@ -26,4 +38,35 @@ pub enum NFCState {
     AwaitingWrite = 0x04,
     ProcessingWrite = 0x05,
     PollAgain = 0x09,
+}
+
+pub fn pack_message(
+    data: &[u8],
+    background: Option<u8>,
+    checksum: Option<fn(&[u8]) -> u8>,
+    length: Option<usize>,
+) -> Vec<u8> {
+    let mut buf: Vec<u8> = data.into();
+    buf.resize(length.unwrap_or(313), background.unwrap_or(0));
+    if buf.len() > length.unwrap_or(313) {
+        warn!("MCU: too long message packed");
+    }
+    let buf_len_without_checksum: usize = buf.len() - 1;
+    buf[buf_len_without_checksum] = checksum.unwrap_or(mcu_crc)(&buf[..(buf_len_without_checksum)]);
+    buf
+}
+
+#[derive(Debug, Clone)]
+pub struct MicroControllerUnit {
+    pub power_state: MCUPowerState,
+    pub nfc_state: NFCState,
+    pub nfc_counter: u32,
+    last_poll_uid: Uuid,
+    pending_active_remove: u32,
+    pub remove_nfc_after_write: bool,
+    controller: Controller,
+    pub seq_no: u32,
+    pub ack_seq_no: u32,
+    pub received_data: Vec<u8>,
+    pub response_queue: Vec<u8>,
 }
